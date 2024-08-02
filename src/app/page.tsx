552fa403206e4391abff22f47f8e474c22e90693
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2024-08-02 15:29:38 -0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+import "@/styles/globals.css";
+
+export default function Home() {
+  return (
+    <div className="wrapper">
+      <div className="marquee-text">
+        <div className="marquee-text-track">
+          <p>Javascript</p>
+          <p>Typescript</p>
+          <p>CSS</p>
+          <p>TailwindCSS</p>
+          <p>Next.js</p>
+          <p>Accessibility</p>
+          <p>React</p>
+          <p>Angular</p>
+        </div>
+      </div>
+    </div>
+  );
+}
